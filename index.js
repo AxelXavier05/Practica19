888d302edc33f329ac6eff6ee5911be0e8d80c6d
AxelXavier05 <axelxaviermorales@gmail.com>
2025-11-12 09:27:40 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-const inicioRoute = require('./routes/inicioRoute');
-
 app.get('/', (req, res) => {
     res.send('Hola NodeJS con Express');
 });
 
 app.listen(port, () => {
     console.log(`Servidor escuchando en http://localhost:${port}`);
-}); 
+});
+
+module.exports = app;
